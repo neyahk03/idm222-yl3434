00cbddf1ab0e90c617c191b8cbd98d6be5b03833
Yen Luong <yl3434@drexel.edu>
2024-10-30 03:16:03 -0400

    :construction: Updated project's case study

---
+++ images/port/cd _files/index.js
@@ -0,0 +1,42 @@
+
+
+const cardList = ['card1', 'card2', 'card3', 'card4'];
+const contentList = ['#content-desktop1', '#content-desktop2', '#content-desktop3', '#content-desktop4'];
+
+function makeActive(x) {
+    const card = x.id;
+    for (let i = 0; i < cardList.length; i++) {
+        if (card == cardList[i]) {
+            console.log(i);
+            const contentHover = document.querySelector(contentList[i]);
+            contentHover.classList.add('active');
+        }
+    }
+}
+
+function undo(x) {
+    const card = x.id;
+    for (let i = 0; i < cardList.length; i++) {
+        if (card == cardList[i]) {
+            console.log(i);
+            const contentHover = document.querySelector(contentList[i]);
+            contentHover.classList.remove('active');
+        }
+    }
+}
+
+
+
+const menuButton = document.querySelector('.btn_menu');
+const menu = document.querySelector('.menu');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('change');
+    menu.classList.toggle('open');
+})
+
+
+
+
+
+
